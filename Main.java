28ab2d8c876e7efef80f63c81388988f943d803f
sylofn <netanelisraelhey@gmail.com>
2025-07-10 13:13:55 +0300

    init

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main{
+    public static Scanner scanner =  new Scanner(System.in);
+    public static void main(String[] args) {
+    }
+
+    public static double aveGrade(int num1, int num2, int num3, int num4){
+        return (num1+num2+num3+num4)/4;
+    }
+
+    public static void meow(){
+        for(int i=0; i>5; i++){
+            System.out.println("enter grade");
+            int z =scanner.nextInt();
+
+        }
+
+    }
+    }
+
+        
+
+
+
